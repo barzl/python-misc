1e60b3238e21e1ce3067df15df3098d294dd31e1
Samuel Zlotnik <samuel.zlotnik@gmail.com>
2015-09-27 10:16:31 +0300

    some refactoring and installing solr to /opt

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,6 @@
-import errno
 import os
-import json
-from fabric.api import env, sudo, put, task, settings, cd
+from fabric.api import env, sudo, put, task, cd
+from fabric.operations import prompt
 import boto
 import boto.ec2
 import time
@@ -78,7 +77,7 @@ def terminate_instance(name):
             else:
                 print instance._state
             print (instance.id, instance.tags['Name'])
-            if raw_input("terminate? (y/n) ").lower() == "y":
+            if prompt("terminate? (y/n) ").lower() == "y":
                 print("Terminating {}".format(instance.id))
                 conn.terminate_instances(instance_ids=[instance.id])
                 print("Terminated")
@@ -86,36 +85,35 @@ def terminate_instance(name):
 
 @task
 def install_solr(name):
-    host_data = get_server_host_data_by_nametag(name)
-    with settings(**host_data):
-        sudo('yum -y install wget')  # To download solr packages
-        sudo('yum -y install java-1.8.0-openjdk-devel.x86_64')  # Latest java
-        sudo('yum -y install lsof.x86_64')  # Requirement by solr to check if already running
 
-        # download and unpack solr
-        with cd('/etc/'):
-            sudo('wget apache.spd.co.il/lucene/solr/5.3.0/solr-5.3.0.tgz')
-            sudo('tar zxf solr-5.3.0.tgz')
-            sudo('rm solr-5.3.0.tgz')
+    set_host_by_name_tag(name)
 
-        # deploy systemd solr startup script
-        with cd('/etc/systemd/system/'):
-            put('solr.service', 'solr.service', use_sudo=True)
-            sudo('systemctl enable solr')
-            sudo('systemctl start solr')
+    sudo('yum -y install wget')  # To download solr packages
+    sudo('yum -y install java-1.8.0-openjdk-devel.x86_64')  # Latest java
+    sudo('yum -y install lsof.x86_64')  # Requirement by solr to check if already running
+
+    # download and unpack solr
+    with cd('/tmp/'):
+        sudo('wget apache.spd.co.il/lucene/solr/5.3.0/solr-5.3.0.tgz')
+        sudo('tar zxf solr-5.3.0.tgz --directory /opt')
+        sudo('rm solr-5.3.0.tgz')
+
+    # deploy systemd solr startup script
+    with cd('/etc/systemd/system/'):
+        put('solr.service', 'solr.service', use_sudo=True)
+        sudo('systemctl enable solr')
+        sudo('systemctl start solr')
 
 
 #  ----------HELPER FUNCTIONS-----------
 
 
-def get_server_host_data_by_nametag(nametag):
+def set_host_by_name_tag(nametag):
     conn = connect_to_ec2()
     filters = {"tag:Name": nametag}
     for reservation in conn.get_all_instances(filters=filters):
         for instance in reservation.instances:
-            host_data = {
-            'host_string': instance.ip_address,
-            'port': env.aws_ssh_port,
-            'user': env.aws_linux_user_name,
-            'key_filename': env.aws_ssh_key_path }
-            return host_data
+            env.host_string = instance.ip_address
+            env.port = env.aws_ssh_port
+            env.user = env.aws_linux_user_name
+            env.key_filename = env.aws_ssh_key_path
